0959d632a88cbec73d88a78e311a35daf289eab9
KiriTamazo Htet Aung <kiritokirikaya80@gmail.com>
2022-07-11 00:52:01 +0630

    Upload redux-crud-branch

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,7 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlicer";
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: {},
 });
--- src/features/counter/counterSlicer.jsx
+++ src/features/counter/counterSlicer.jsx
@@ -1,30 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  count: 0,
-};
+const initialState = {};
 
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {
-    increment: (state) => {
-      state.count += 1;
-    },
-    decrement: (state) => {
-      state.count -= 1;
-    },
-    reset: (state) => {
-      state.count = 0;
-    },
-    incrementAmout: (state, action) => {
-      state.count += action.payload;
-    },
-    decrementAmout: (state, action) => {
-      state.count -= action.payload;
-    },
-  },
+  reducers: {},
 });
-export const { increment, decrement, reset, incrementAmout, decrementAmout } =
-  counterSlice.actions;
-export default counterSlice.reducer;
+export const {} = actions;
+export default reducer;
